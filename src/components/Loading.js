4c9f30c1fbd7c9331dde64a996d1af16cea5aa82
Jidugu Tejesh <tejeshjidugu@gmail.com>
2025-11-08 00:32:28 +0530

    Create Loading.js

---
+++ src/components/Loading.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Loading() {
+  return (
+    <div className="loading">
+      <div className="spinner" />
+      <p>Loading…</p>
+    </div>
+  );
+}
+
+export default Loading;
